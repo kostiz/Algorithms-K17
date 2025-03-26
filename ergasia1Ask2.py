a115ea0b3b91187bf8a1d680c944275745e82e41
Kostiz <69690531+kostiz@users.noreply.github.com>
2025-03-26 15:19:42 +0200

    Add files via upload

--- ergasia1Ask2.py
+++ ergasia1Ask2.py
@@ -1,15 +1,24 @@
 def findMissing(array):
-    full_array = array + [0]
-    print(full_array)
-    for i in range(0,len(full_array)):
-        if i+1 != full_array[i]:
-            return i+1
+    left = 0
+    right = len(array)
+    mid = 0
+    while array[mid] != right or mid != left:
+        mid = (left + right) // 2
+        if mid + 1 >= array[mid]:
+            left = mid
+        else:
+            if left == right == 0:
+                return 1
+            right = mid
+    return array[mid] + 1
 
 
-A = [1,2,3,5,6,7,8,9,10]
-B = [1,2,3,4,5,7,8,9,10]
-C = [1,2,3,4,5,6,7,8,9]
-D = [2,3,4,5,6,7,8,9,10]
-E = [1,2,3,4,6,7,8,9,10]
+A = [1,2,3,5,6,7,8,9,10] #4 is missing
+B = [1,2,3,4,5,7,8,9,10] #6 is missing
+C = [1,2,3,4,5,6,7,8,9]  #10 is missing
+D = [2,3,4,5,6,7,8,9,10] #1 is missing
+E = [1,2,3,4,6,7,8,9,10] #5 is missing
+F = [1,2,3,4,5,6,7,8,10] #9 is missing
+G = [1,3,4,5,6,7,8,9,10] #2 is missing
 
-print(findMissing(A),findMissing(B),findMissing(C),findMissing(D),findMissing(E))
+print(findMissing(A), findMissing(B),findMissing(C),findMissing(D),findMissing(E),findMissing(F),findMissing(G))
